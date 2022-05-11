8784a41048624550545354cf9ec709c7c16ef732
刘夏 <liuxia@sansi.com>
2022-05-11 17:32:00 +0800

    refactor(settings): 优化修改智能匹配规则反馈体验

--- src/settings-match.tsx
+++ src/settings-match.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { PrimaryButton, TextField, Stack } from '@fluentui/react';
+import { PrimaryButton, TextField, Stack, MessageBar } from '@fluentui/react';
 import { getRulesString, saveRulesString } from './util';
 
 
@@ -11,22 +11,29 @@ export function SettingsMatch() {
     getRulesString().then(setRules);
   }, [])
   async function commit(rules: string) {
-    await saveRulesString(rules);
+    saveRulesString(rules);
+    setDisabled(true);
   }
+  const [disabled, setDisabled] = useState(true);
+
   return (
     <Stack tokens={{
       childrenGap: 20
     }}>
       <TextField label="智能匹配" multiline value={rules} rows={10} onChange={(e, value) => {
         setRules(value);
+        setDisabled(false);
       }} />
-      <PrimaryButton text='保存' styles={{
+      <PrimaryButton text='保存' disabled={disabled} styles={{
         root: {
           width: '100px',
         }
       }} onClick={() => {
         commit(rules);
       }} />
+      <MessageBar>
+        注意：智能匹配只会匹配第一个条件，如果有多个条件，只会匹配第一个条件。
+      </MessageBar>
     </Stack>
   );
 }
