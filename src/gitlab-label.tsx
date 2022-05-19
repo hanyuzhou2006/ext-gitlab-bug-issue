5a4c4cabe4c1908da3fca5492c6f26b3ecef5dd3
刘夏 <liuxia@sansi.com>
2022-05-19 14:34:58 +0800

    feat(settings): scoped label

--- src/gitlab-label.tsx
+++ src/gitlab-label.tsx
@@ -6,18 +6,70 @@ export type LabelProp = {
   textColor: string;
 }
 export function GitlabLabel(props: LabelProp): JSX.Element {
-  const { name, color, textColor } = props;
-  return <div style={{
-    display: 'inline-block',
-    padding: '2px 4px',
-    marginRight: '4px',
-    borderRadius: '2px',
-    backgroundColor: color,
-    color: textColor,
-    fontSize: '0.8em',
-    fontWeight: 'bold',
-  }}>{name}</div>
+  const { name: labelName, color, textColor } = props;
+  // if name is a scope, then render a scoped label
+  if (labelName.includes('::')) {
+    const [scope, name] = labelName.split('::');
+    return (
+      <GitlabScopedLabel scope={scope} name={name} color={color} textColor={textColor} />
+    );
+  }
+
+  return <GitlabCommonLabel name={labelName} color={color} textColor={textColor} />
 }
 
+const labelStyle: React.CSSProperties = {
+  display: 'inline-flex',
+  alignItems: 'center',
+  overflow: 'hidden',
+  borderRadius: '0.75rem',
+  color: '#fff',
+  fontSize: '0.875em',
+  position: 'relative',
+  lineHeight: '16px',
+}
 
+const labelNameStyle: React.CSSProperties = {
+  paddingLeft: '0.5rem',
+  paddingRight: '0.5rem',
+  paddingTop: '0.25rem',
+  paddingBottom: '0.25rem',
+  display: 'inline-block',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  verticalAlign: 'top',
+  whiteSpace: 'nowrap',
+  maxWidth: '100%',
+}
+
+function GitlabCommonLabel(props: LabelProp): JSX.Element {
+  const { name, color, textColor } = props;
+  return <span style={{
+    ...labelStyle,
+    backgroundColor: color,
+    color: textColor,
+  }}>
+    <span style={labelNameStyle}>{name}</span>
+  </span>
+}
+
+function GitlabScopedLabel(props) {
+  const { scope, name, color, textColor } = props;
+  return <span style={{
+    ...labelStyle,
+    color: textColor,
+    boxShadow: `0 0 0 2px ${color} inset`,
+  }}>
+    <span style={{
+      ...labelNameStyle,
+      backgroundColor: color,
+      paddingRight: '0.25rem',
+    }}>{scope}</span>
+    <span style={{
+      ...labelNameStyle,
+      paddingLeft: '0.25rem',
+      color: '#000',
+    }}>{name}</span>
+  </span>
+}
 
